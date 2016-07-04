2619e6c1025f9ea075e3a5cf5a0da3b2fe85b212
Your Name <you@example.com>
2016-07-04 09:55:10 +0000

    .

--- mysql-patch/20_addtable.sql
+++ mysql-patch/20_addtable.sql
@@ -3,7 +3,7 @@ Author - ABC
 Created Date - 06/18/2015
 Purpose - Update  default values abc and efg 
 */
-create table penguins (id int primary key, myval varchar(50)); 
+create table opstree (id int primary key, myval varchar(50)); 
 /*
 commit
 */
