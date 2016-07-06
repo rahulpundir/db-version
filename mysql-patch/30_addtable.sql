951648630448ca8da8c383e7561b28ef6dc39207
Your Name <you@example.com>
2016-07-06 07:27:53 +0000

    .

---
+++ mysql-patch/30_addtable.sql
@@ -0,0 +1,10 @@
+/*
+Author - ABC
+Created Date - 06/18/2015
+Purpose - Update  default values abc and efg 
+*/
+create table opstreeSolutionz (id int primary key, myval varchar(50)); 
+/*
+commit
+*/
+
